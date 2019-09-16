61022bb2cb484d52582da3581b7dfa72f1bc47a5
Patrick Koch <p.koch@tum.de>
2019-09-15 22:02:44 -0300

    print basic environment property

---
+++ monopoly/environment.py
@@ -0,0 +1,5 @@
+
+class Environment():
+	"""Defines the environment of the multiagent system."""
+	def __init__(self):
+		self.numOfBusinessmen = 20
---
+++ monopoly/run.py
@@ -0,0 +1,6 @@
+from environment import Environment
+
+
+if __name__	== "__main__":
+	environment = Environment()
+	print(environment.numOfBusinessmen)
