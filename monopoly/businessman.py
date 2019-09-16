4ae61d86949244cc451b94b643321d1d821158d8
Patrick Koch <p.koch@tum.de>
2019-09-15 22:18:30 -0300

    businessman class initialization

--- monopoly/businessman.py
+++ monopoly/businessman.py
@@ -8,5 +8,6 @@ class Businessman():
 		self.capital = 1000 * np.random.randn() + 10000		# sig * randn + mu
 		self.happiness = 10 * np.random.randn() + 50		# sig * randn + mu
 		self.isAlive = True
-		self.subsidiaries = 0
+		self.subsidiaries = 500000 * 1 / (self.capital + 1000)
 		self.companies = []
+		
