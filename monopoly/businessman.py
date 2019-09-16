138b259345603b4cca7397bdfa083d651d578fd1
Patrick Koch <p.koch@tum.de>
2019-09-15 21:30:19 -0300

    Add company file

---
+++ monopoly/businessman.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Businessman():
+	"""An intelligent agent that interacts with other agents with the objective
+	to maximize his capital."""
+
+	def __init__(self):
+
+		self.capital = 1000 * np.random.randn() + 10000		# sig * randn + mu
+		self.happiness = 10 * np.random.randn() + 50		# sig * randn + mu
+		self.isAlive = True
+		self.subsidiaries = 0
+		self.companies = []
+
