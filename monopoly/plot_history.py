cc713a7d6ff89587829f12477e6c9c7dba36665a
neto22 <pneto.98@gmail.com>
2019-09-27 23:35:57 -0300

    plot separated

--- monopoly/plot_history.py
+++ monopoly/plot_history.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import CheckButtons
+from matplotlib.widgets import TextBox
 import numpy as np
 
 
@@ -15,6 +17,9 @@ ax2 = fig.add_subplot(2,1,2)
 ax2.set_title('Profit History')
 ax2.set_xlabel('Time (days)')
 ax2.set_ylabel('Profit (USD)')
+
+initial_text = ""
+active = []
 
 def plot_all(peopleCapitalDict, peopleProfitDict, numDays):
 
@@ -32,20 +37,29 @@ def plot_all(peopleCapitalDict, peopleProfitDict, numDays):
     profitArr = np.array(profitList)[bmIds]
     totalDailyProfits = np.sum(profitArr, 2)
     colors = cm.gnuplot(np.linspace(0.1, 0.8, len(bmIds)))
+    
 
     days = np.arange(1, numDays + 1)
+    l2 = []
 
     def update(day):
 
 
         ax1.clear()
+        ax2.clear()
         # labels = [('BM' + str(bm.id)) for bm in businessmen]
         l1 = ax1.pie(capitalArr[:,day], autopct='%1.1f%%', shadow=True, startangle=90, colors=colors)
         ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
         ax1.set_title('Day ' + str(day+1))
 
         for j, clr in zip(range(len(bmIds)), colors):
-            l2 = ax2.plot(days[0:day], totalDailyProfits[j, 0:day], color=clr, label=j)
+            l, = ax2.plot(days[0:day], totalDailyProfits[j, 0:day], color=clr, label=j)
+            l2.append(l)
+
+            if l.get_label() not in active:
+                l.set_visible(False)
+            else:
+                l.set_visible(True)
 
         fig.legend([l1, l2],     # The line objects
            labels=line_labels,   # The labels for each line
@@ -57,6 +71,17 @@ def plot_all(peopleCapitalDict, peopleProfitDict, numDays):
         plt.pause(pause_interval)
 
 
+    def submit(text):
+        global active
+        if text not in active:
+            active.append(text)
+        else: 
+            active.remove(text)
+        
 
-    anim = FuncAnimation(fig, update, frames=range(numDays), repeat=False)   
+    anim = FuncAnimation(fig, update, frames=range(numDays), repeat=False)
+    axbox = plt.axes([0.1, -0.01, 0.8, 0.075])
+    text_box = TextBox(axbox, 'BM no:', initial=initial_text)
+    text_box.on_submit(submit)
+
     plt.show()
