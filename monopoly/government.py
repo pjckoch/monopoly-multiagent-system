e902aaa835d44aef81f0a232596cb12b4736234c
Francisco Ferreira <francisco.tf4@gmail.com>
2019-11-11 22:43:49 -0300

    Add Subsidiaries Formula

--- monopoly/government.py
+++ monopoly/government.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from enum import Enum
 import data_manager
 
@@ -53,8 +54,8 @@ class Government():
 
         return sortedList
 
+    # Returns the percentage of government money for subsidiaries that a bm will receive
     def calculateSubsidiaryPercentage(self, index, bmList):
-        # Returns the percentage of government money for subsidiaries that a bm will receive
         inverseList = bmList[::-1]
     
         totalBmMoney = 0
@@ -63,72 +64,42 @@ class Government():
 
         return inverseList[index].capital/totalBmMoney
 
+    def calculateSubsidiaryFormula(self, capital):
+        return 4000-100*np.log(capital+1)
 
+    # Give Equal Subsidiaries to Everyone
     def calculateSubsidiary(self, bm, govMon, noBm):
         return govMon/len(bmList)
 
+    # Give Differentiated Subsidiaries to Everyone
     def calculateSubsidiaryOnSteroids(self, bm, govMon, bmList):
         sortedList = self.sortListByCapital(bmList)
         index = sortedList.index(bm)
         return govMon * self.calculateSubsidiaryPercentage(index, bmList)
 
-    #TODO: improve
+    # Give Differentiated Subsidiaries to Poor People When They are Below 2000$
+    def robinHoodMode(self, bm):
+        if bm.capital < 2000:
+            return self.calculateSubsidiaryFormula(bm.capital)
+        return 0
+
+
     def regulateSubsidiary(self,businessman,govMon, bmList):
-        subs = self.calculateSubsidiaryOnSteroids(businessman,govMon,bmList)
+        subs = self.robinHoodMode(businessman)
         businessman.subsidiariesHistory.append(subs)
         businessman.capital += subs
         self.governmentMoney -= subs
         print("BM " + str(businessman.id) + " received " + str(subs))
-        # if not self.isPersonHelped(businessman):
-        #     # if (businessman.capital < (self.avgCapital * 0.5)):
-        #     if businessman.capital < 10000 and self.governmentMoney - self.subsidyValue > 0:
-        #         businessman.subsidiaries += self.subsidyValue
-        #         self.governmentMoney -= self.subsidyValue
-        #         temp = [businessman.id, 0] # We put a pair instead of ID only because later on we will increment the counter to 3 days per subsidiary
-        #         self.subsidiariesStatus.append(temp)
-        # else:
-        #     for subs in self.subsidiariesStatus:
-        #         if subs[0] == businessman.id:
-        #             self.subsidiariesStatus.remove(subs)
-        #             businessman.subsidiaries = 0
 
-    # def isCompanyTaxed(self, company):
-    #     for tax in self.taxesStatus:
-    #         if tax[0] == company.id:
-    #             return True
-    #     return False
-
-    # def ejectCapital(self,bm):
-    #     if self.governmentMoney:
-    #         if bm.capital < 7000 and self.politics == PoliticsSwitcher.SUPPORTIVE:
-    #             bm.companies[0].investmentLevel = bm.companies[0].investmentLevel + 1
-    #         if bm.capital < 5000 and self.politics == PoliticsSwitcher.NEUTRAL:
-    #             bm.capital = 5000 + random.randint(0, 400)
-    #         if bm.capital < 1500 and self.politics == PoliticsSwitcher.COMPETITIVE:
-    #             bm.capital = 0 + random.randint(0, 1500)
-
-    #TODO: improve
     def regulateTax(self,company):
         if company.bruttoProfitHistory[-1] > 0:
             company.taxHistory.append(company.bruttoProfitHistory[-1] * self.taxRate)
             self.governmentMoney += company.taxHistory[-1]
-        # if not self.isCompanyTaxed(company):
-        #     if company.companyValue > self.avgCompanyValue:
-        #         company.taxes += self.taxRate
-        #         self.governmentMoney += self.taxRate
-        #         temp = [company.id, 0]
-        #         self.taxesStatus.append(temp)
-        # else:
-        #     for tax in self.taxesStatus:
-        #         if tax[0] == company.id:
-        #             self.taxesStatus.remove(tax)
-        #             company.taxes = 0
 
     def regulate(self, averageCapital, averageCompany, businessmenList):
         momentaryGovMoney = self.governmentMoney
         for bm in businessmenList:
             self.regulateSubsidiary(bm, momentaryGovMoney, businessmenList)
-            # self.ejectCapital(bm)
             for company in bm.companies:
                 self.regulateTax(company)
 
