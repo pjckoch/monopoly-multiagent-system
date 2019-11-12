425c38634ec2e57cc09f787cafbae52265ae74e7
Patrick Koch <pjc.koch@gmail.com>
2019-11-12 04:36:57 -0300

    Add transaction function

---
+++ monopoly/helper_funs.py
@@ -0,0 +1,15 @@
+import government
+import businessman
+import company
+
+def transaction(sender, receiver, amount):
+    if type(sender) is businessman.Businessman:
+        sender.capital -= amount
+    elif type(sender) is government.Government:
+        sender.governmentMoney -= amount
+    if type(receiver) is company.Company:
+        receiver.turnOver += amount
+    elif type(receiver) is government.Government:
+        receiver.governmentMoney += amount
+    else:
+        receiver.capital += amount
